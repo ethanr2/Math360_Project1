061eab1b412e86076f344b42eb2e95be68a3b69f
ethanr2 <ethanrhmn@gmail.com>
2020-09-17 14:49:29 -0500

    working on vector field

--- charts.py
+++ charts.py
@@ -1,4 +1,3 @@
-
 # -*- coding: utf-8 -*-
 
 from datetime import date
@@ -9,12 +8,13 @@ import numpy as np
 import pandas as pd
 from scipy import stats
 
-from bokeh.io import export_png,output_file,show
+from bokeh.io import export_png, output_file, show
 from bokeh.plotting import figure
-from bokeh.models import LinearColorMapper, LabelSet,ColumnDataSource
+from bokeh.models import LinearColorMapper, LabelSet, ColumnDataSource,Arrow, NormalHead, OpenHead, VeeHead
 from bokeh.models.tickers import FixedTicker
 from bokeh.layouts import row, column
 from bokeh.palettes import Viridis256
+
 # Parameters
 
 START_YEAR = 1990
@@ -22,207 +22,207 @@ POP_MAX = 10000
 a = 0.2
 b = 0.001
 c = 0.3
-d = .002
+d = 0.002
 ts = []
 
-def trial(x_0 = 149, y_0 = 201, num_iter = False, display = False):
+def model(x, y):
+    dX = - a * x + b * x * y
+    dY = - c * y + d * x * y
+    return (dX, dY)
+def trial(x_0=149, y_0=201, display=False, num_iter=False, pop_exit= True):
     X = [x_0]
     Y = [y_0]
+    dX, dY = model(X[-1], Y[-1])
+    dX = [dX]
+    dY = [dY]
     iterate = True
     t = 0
     equilibrium = False
-    while iterate:
-        X_next = X[-1] - a*X[-1] + b*X[-1]*Y[-1]
-        Y_next = Y[-1] - c*Y[-1] + d*X[-1]*Y[-1]
-        
+    while iterate:        
+        #print(X[-1],dX )
+        X_next = X[-1] + dX[-1]
+        Y_next = Y[-1] + dY[-1]
+        dX_next, dY_next = model(X_next, Y_next)
+       
         X.append(X_next)
         Y.append(Y_next)
-        #print(t+START_YEAR, X_next, Y_next)
+        dX.append(dX_next)
+        dY.append(dY_next)
+        
         if t > 1:
-            equilibrium = (X[-1] == X[-2]) and (Y[-1] == Y[-2])   
+            equilibrium = (X[-1] == X[-2]) and (Y[-1] == Y[-2])
         if min(X_next, Y_next) < 0.5 or equilibrium:
-            color = np.nan 
-            iterate = False 
-        if max(X_next, Y_next) > POP_MAX:
+            color = np.nan
+            iterate = False
+        if pop_exit and max(X_next, Y_next) > POP_MAX:
             color = t
             iterate = False
-        if not num_iter and t >= num_iter:
+        if num_iter and t >= num_iter:
             color = t
             iterate = False
+        if display:
+            if max(X_next, Y_next) < 1000000:
+                print('{} {:10.2f} {:10.2f}'.format(t+START_YEAR, X_next, Y_next))
+            else:
+                print('{} {:10.2e} {:10.2e}'.format(t+START_YEAR, X_next, Y_next))
         t += 1
-    return {
-        'X': X,
-        'Y': Y,
+    data = {
+        "X": X, 
+        "dX": dX,
+        "Y": Y, 
+        "dY": dY,
         't': color
         }
     
-def make_matrix(n, X_max =300, Y_max = 300):
+    return data
+#trial(200,300, display = True, num_iter = 17, pop_exit = False)
+#%%
+def make_matrix(n, X_max=300, Y_max=300):
     x_noughts = []
+    dx_noughts = []
     X = []
     y_noughts = []
+    dy_noughts = []
     Y = []
     ts = []
-    step = X_max/n
-    arr_i= []
-    for i in range(1,n):
-        arr_j= []
-        for j in range(1,n):
-            arr_j.append((i*step,j*step))
-            rez = trial(i*step,j*step)
-            x_noughts.append(rez['X'][0])
-            y_noughts.append(rez['Y'][0])
-            X.append(rez['X'])
-            Y.append(rez['Y'])
+    step = X_max / n
+    arr_i = []
+    vect_colors = []
+    for i in range(1, n):
+        arr_j = []
+        for j in range(1, n):
+            arr_j.append((i * step, j * step))
+            rez = trial(i * step, j * step)
+            x_noughts.append(rez["X"][0])
+            y_noughts.append(rez["Y"][0])
+            dx_noughts.append(rez["dX"][0])
+            dy_noughts.append(rez["dY"][0])
+            X.append(rez["X"])
+            Y.append(rez["Y"])
             ts.append(rez['t'])
-            print(rez['t'])
+            vect_colors.append(np.sqrt(rez["dX"][0]**2 + rez["dY"][0]**2))
+            print(i,j)
         arr_i.append(arr_j)
-    df = pd.DataFrame({
-        'X_0': x_noughts,
-        'X': X,
-        'Y_0': y_noughts,
-        'Y': Y,
-        'color': ts
-        })
-    
+    data = {
+        "X_0": x_noughts, 
+        'dX_0': dx_noughts,
+        "X": X, 
+        "Y_0": y_noughts,
+        "dY_0": dy_noughts,
+        "Y": Y, 
+        "color": ts,
+        'vector_color': vect_colors
+        }
+    df = pd.DataFrame(data)
+   
     # Replaces NA values
-    df.loc[df['color'].isna(), 'color'] = df['color'].max()
-    
-    hist = np.histogram(df['color'], density = True, bins = 50)
+    df.loc[df["color"].isna(), "color"] = df["color"].max()
+
+    hist = np.histogram(df["color"], density=True, bins=50)
     transform = lambda x: np.searchsorted(hist[1], x)
-    df.loc[:, 'color'] =df.loc[:, 'color'].apply(transform) 
+    df.loc[:, "color"] = df.loc[:, "color"].apply(transform)
     print(df)
     return df
 
-
-
-df = make_matrix(20, 300, 300)
-source = ColumnDataSource(df)
+#df = make_matrix(500, 500, 500)
+df = make_matrix(25, 500, 500)
 
 #%%
-#trial(200,300)
-p = figure(width = 1000, height = 1000,
-           title="Math 360 Project 1" , 
-           x_axis_label = 'X', 
-           y_axis_label = 'Y',
-           y_range = (0,500), x_range=(0,500)
-           #x_axis_type = 'log', y_axis_type = 'log'
-           )
-
-#p.line(xrng,[0,0], color = 'black')
-#p.line([0,0],yrng, color = 'black')
-mapper = LinearColorMapper( palette=Viridis256, low=df['color'].max(), high=df['color'].min())
-colors= { 'field': 'color', 'transform': mapper}
-
-# row = trial(100,200)
-# xdata = row['X']
-# ydata = row['Y']
-# p.line(xdata, ydata, color = 'black')
-df= make_matrix2(25, 500,500)
-for i,row in df.iterrows():
-    xdata = row['X_0']
-    ydata = row['Y_0']
+def histogram_coloring():
+    source = ColumnDataSource(df)
+    p = figure(
+        width=800,
+        height=800,
+        title="Histogram Coloring of Initial Points",
+        x_axis_label="X",
+        y_axis_label="Y",
+        y_range=(0, 500),
+        x_range=(0, 500),
+    )
+    
+    mapper = LinearColorMapper(
+        palette=Viridis256, low=df["color"].max(), high=df["color"].min()
+    )
+    colors = {"field": "color", "transform": mapper}
+    
+    p.circle(x='X_0',y= 'Y_0', color = colors, source = source, size = 1.5)
+    p.xaxis[0].ticker.desired_num_ticks = 10
+    
+    p.xgrid.grid_line_color = None
+    p.ygrid.grid_line_color = None
+    name = str(dt.now())[:19].replace(" ", "").replace(":", "-")
+    export_png(p, filename="imgs/histo_coloring{}.png".format(name))
+    
+    return p
+def phase_space(x_0 = 100, y_0 = 200):
+    p = figure(
+        width=800,
+        height=800,
+        title="Phase Space: X = {}, Y = {}".format(x_0, y_0),
+        x_axis_label="X",
+        y_axis_label="Y",
+        y_range=(0, 300),
+        x_range=(0, 300)
+    )
+    
+    mapper = LinearColorMapper(
+        palette=Viridis256, low=df["color"].max(), high=df["color"].min()
+    )
+    colors = {"field": "color", "transform": mapper}
+    
+    row = trial(x_0,y_0)
+    xdata = row['X']
+    ydata = row['Y']
     p.line(xdata, ydata, color = 'black')
-#p.circle(x='X_0',y= 'Y_0', color = colors, source = source, size = 2)
-p.xaxis[0].ticker.desired_num_ticks = 10
-p.xgrid.grid_line_color = None
-p.ygrid.grid_line_color = None
-#p.xaxis.formatter=NumeralTickFormatter(format="0.0%")
-#p.yaxis.formatter=NumeralTickFormatter(format="0.0%")
-
-name = str(dt.now())[:19].replace(' ','').replace(':','-')
-export_png(p, filename = 'imgs/{}.png'.format(name))
-show(p)
-
-#%%
-
-df
-#%%
-def set_up(x, y, truncated = True, margins = None):
-    if truncated: 
-        b = (3 * y.min() - y.max())/2
-    else:
-        b = y.min()
-    if margins == None:    
-        xrng = (x.min(),x.max())
-        yrng = (b,y.max())
-    else:
-        xrng = (x.min() - margins,x.max() + margins)
-        yrng = (b - margins,y.max() + margins)
-        
-    x = x.dropna()
-    y = y.dropna()
-    
-    return(x,y,xrng,yrng)
-
-# Chart of non-stationary time series, e.g. NGDP from 2008 to 2020    
-def chart0(df):
-    xdata, ydata, xrng, yrng = set_up(df.index,df['___'])
-    
-    p = figure(width = 1000, height = 500,
-               title= '____', 
-               x_axis_label = 'Date', x_axis_type = 'datetime',
-               y_axis_label = '', 
-               y_range = yrng, x_range = xrng)
-    p.line(xrng,[0,0], color = 'black')
-    
-    p.line(xdata,ydata, color = 'blue', legend = '')
-    
-    p.xaxis[0].ticker.desired_num_ticks = 10
-    p.legend.location = 'top_left'
-    p.ygrid.grid_line_color = None
-    p.yaxis.formatter=NumeralTickFormatter(format="____")
-    
-    export_png(p,filename ='imgs/chart0.png')
-
-    return p
-
-
-# Chart of approximately stionary time series, e.g. PCE-Core inflation from 2008 to 2020
-def chart1(df):
-    xdata, ydata, xrng, yrng = set_up(df.index, df['__'], truncated = False)
-    
-    p = figure(width = 1000, height = 500,
-               title="_" , 
-               x_axis_label = 'Date', x_axis_type = 'datetime',
-               y_axis_label = '_', 
-               y_range = yrng, x_range = xrng)
-    p.line(xrng,[0,0], color = 'black')
-    
-    p.line(xdata,ydata, color = 'blue', legend = '_')
-    
-    p.xaxis[0].ticker.desired_num_ticks = 10
-    p.legend.location = 'bottom_right'
-    p.ygrid.grid_line_color = None
-    p.yaxis.formatter=NumeralTickFormatter(format="0.0%")
-
-    export_png(p, filename='imgs/chart1.png')
-
-    return p
-
-# Chart of a regression e.g. inflation vs money supply
-def chart2(df):
-    xdata, ydata, xrng, yrng = set_up(df['_'], df['_'], 
-                                      truncated = False, margins = .005)
-    
-    p = figure(width = 500, height = 500,
-               title="_" , 
-               x_axis_label = '_', 
-               y_axis_label = '_', 
-               y_range = yrng, x_range = xrng)
-    p.line(xrng,[0,0], color = 'black')
-    p.line([0,0],yrng, color = 'black')
-    
-    slope, intercept, r_value, p_value, std_err = stats.linregress(xdata, ydata)
-    leg = 'R = {:.4f}, P-Value = {:.4e}, Slope = {:.4f}'.format(r_value,p_value,slope)
-    p.line(xdata, xdata*slope + intercept, legend = leg, color = 'black')
-    p.circle(xdata,ydata, color = 'blue',size = 2)
+    # for i, row in df.iterrows():
+    #     xdata = row["X_0"]
+    #     ydata = row["Y_0"]
+    #     p.line(xdata, ydata, color="black")
     
     p.xaxis[0].ticker.desired_num_ticks = 10
     p.xgrid.grid_line_color = None
     p.ygrid.grid_line_color = None
-    p.xaxis.formatter=NumeralTickFormatter(format="0.0%")
-    p.yaxis.formatter=NumeralTickFormatter(format="0.0%")
-    
-    export_png(p, filename='images/chart2.png' )
+
+    name = str(dt.now())[:19].replace(" ", "").replace(":", "-")
+    export_png(p, filename="imgs/phase_space{}.png".format(name))
     
     return p
+
+def vector_field(size = 20):
+    df['X_1'] = df['X_0'] + df['dX_0']/df['vector_color']*size
+    df['Y_1'] = df['Y_0'] + df['dY_0']/df['vector_color']*size
+    source = ColumnDataSource(df)
+    p = figure(
+        width=800,
+        height=800,
+        title="Vector Field",
+        x_axis_label="X",
+        y_axis_label="Y",
+        y_range=(0, 500),
+        x_range=(0, 500)
+    )
+    
+    mapper = LinearColorMapper(
+        palette=Viridis256, low=df["vector_color"].max(), high=df["vector_color"].min()
+    )
+    colors = {"field": "vector_color", "transform": mapper}
+
+
+    # for i, row in df.iterrows():
+    #     x = row["X_0"]
+    #     y = row["Y_0"]
+    #     dx = row['dX_0']
+    #     dy = row['dY_0']
+    p.add_layout(Arrow(end=NormalHead(size = 7, fill_color = colors, source= source),
+                       x_start='X_0', y_start='Y_0', x_end='X_1', y_end='Y_1', source = source,
+                       line_color = colors))
+    
+    p.xaxis[0].ticker.desired_num_ticks = 10
+    p.xgrid.grid_line_color = None
+    p.ygrid.grid_line_color = None
+
+    name = str(dt.now())[:19].replace(" ", "").replace(":", "-")
+    export_png(p, filename="imgs/vector_field{}.png".format(name))
+    
+    return p
+show(vector_field())
